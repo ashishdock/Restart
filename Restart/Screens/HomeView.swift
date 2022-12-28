a0f0269cf048830212f21c6514ee38e9f6d4a537
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-28 19:09:52 +0530

    Finishing touches remaining.

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -11,18 +11,54 @@ struct HomeView: View {
     
     //MARK: - PROPERTY
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
-    
+    @State private var isAnimating: Bool = false
     
     //MARK: - BODY
     var body: some View {
-        VStack{
-        Text("Home").font(.largeTitle)
-            Button(action: {
-                isOnboardingViewActive = true
-            }) {
-                Text("Restart")
+        VStack(spacing: 20){
+// MARK: - HEADER
+            Spacer()
+            ZStack {
+                CircleGroupView(shapeColor: .purple, shapeOpacity: 0.1)
+                Image("character-2")
+                .resizable()
+                .scaledToFit()
+                .padding()
+                .offset(y: isAnimating ? 35 : -35)
+                .animation(Animation.easeOut(duration: 4).repeatForever(), value: isAnimating)
             }
+            
+//MARK: - CENTER
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+//MARK: - FOOTER
+
+            Spacer()
+            Button(action: {
+                withAnimation {
+                    isOnboardingViewActive = true
+                }
+            }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }//: BUTTON
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(ControlSize.large)
         }//: VSTACK
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                isAnimating = true
+            }
+        }
     }
 }
 
