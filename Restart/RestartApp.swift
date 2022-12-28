ef3dceea5fd841b90205533558af2682fbcdc3c4
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-28 12:28:10 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Ashish Sharma on 12/28/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
